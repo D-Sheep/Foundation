c1b50cff77a53746dd12d6c5c911996150ff807f
jurajhrib <jurajhrib@gmail.com>
2014-11-14 11:04:25 +0100

    added locator interface

---
+++ Localization/ILocatorService.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jurajhrib
+ * Date: 20.9.2014
+ * Time: 14:12
+ */
+
+namespace Foundation\Localization;
+
+interface ILocatorService {
+
+    public function getUserLanguage();
+
+    public function getUserCity();
+
+    public function getUserCountry();
+
+}
